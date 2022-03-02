63c79a82534182cf1206008140efa6f673f6b374
gudivadasusmitha <Gudivada.Susmitha@ibm.com>
2022-03-02 11:47:03 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace passing_array
+{
+    internal class Program
+    {
+        static void Result(int[] arr)
+        {
+            for(int i=0;i<arr.Length;i++)
+            {
+                Console.WriteLine("array element: " + arr[i]);
+
+            }
+
+        }
+        public static void Main()
+        {
+            int[] arr = { 1, 2, 3, 4, 5 };
+            Result(arr);
+            Console.ReadLine();
+
+        }
+    }
+}
